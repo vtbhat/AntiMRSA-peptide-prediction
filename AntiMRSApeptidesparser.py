7f948d9b663c9ba58f569e7a6293ce2ac320b0c0
vtbhat <82274960+vtbhat@users.noreply.github.com>
2021-05-20 13:00:41 +0530

    Add files via upload

---
+++ AntiMRSApeptidesparser.py
@@ -0,0 +1,17 @@
+import re
+import pandas as pd
+
+file1= open("C:/Users/VARSHA BHAT/Downloads/Queryresults.txt", "r").readlines()
+dictofmrsa = {}
+
+for i in file1:
+    APids = re.search("AP\d\d\d\d\d", i)#Find APD ID
+    if APids != None:
+        APidind = APids.group()#Add APD ID as a key in the dictionary
+        dictofmrsa[APidind] = ""
+    peptideseq = re.search("(?<=\t).+?(?=\s)", i)#Find correspoding amino acid sequence of the peptide
+    if peptideseq != None:
+        peptideseqind = peptideseq.group()
+        dictofmrsa[APidind] = peptideseqind#Add the peptide sequence as a value in the dictionary
+
+(pd.DataFrame.from_dict(data=dictofmrsa, orient='index').to_csv('C:/Users/VARSHA BHAT/Documents/MRSApeptides.csv', header=False))#Convert the dictionary to CSV
